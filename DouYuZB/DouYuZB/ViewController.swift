c475851329b07bb8263215c560afc5db2deaa0e0
dulu <lu.du@joy2u.cn>
2017-06-15 14:47:50 +0800

    项目初始化

---
+++ DouYuZB/DouYuZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouYuZB
+//
+//  Created by DuLu on 15/06/2017.
+//  Copyright © 2017 DuLu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DouYuZB/DouYuZBTests/DouYuZBTests.swift
@@ -0,0 +1,36 @@
+//
+//  DouYuZBTests.swift
+//  DouYuZBTests
+//
+//  Created by DuLu on 15/06/2017.
+//  Copyright © 2017 DuLu. All rights reserved.
+//
+
+import XCTest
+@testable import DouYuZB
+
+class DouYuZBTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
